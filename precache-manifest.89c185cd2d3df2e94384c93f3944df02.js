1d1908d4d616873ba56d38b115fed025c8cbbe52
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-17 11:38:55 +0200

    Updates

---
+++ precache-manifest.89c185cd2d3df2e94384c93f3944df02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea137931d9fa6eb3c1d26ea3d32ead",
+    "url": "/react-context-cart/index.html"
+  },
+  {
+    "revision": "946acdae0cf7849314d9",
+    "url": "/react-context-cart/static/css/main.cc390d1e.chunk.css"
+  },
+  {
+    "revision": "b575e1d2735eee215676",
+    "url": "/react-context-cart/static/js/2.15070a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-context-cart/static/js/2.15070a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946acdae0cf7849314d9",
+    "url": "/react-context-cart/static/js/main.39685d08.chunk.js"
+  },
+  {
+    "revision": "c63aa7541b267c183c68",
+    "url": "/react-context-cart/static/js/runtime-main.5e5e9eb9.js"
+  }
+]);
